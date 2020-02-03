075e50ee7cd5a1d5ccaa342a3c23639ea480d28f
absaki <28054064+absaki@users.noreply.github.com>
2020-02-03 22:14:12 +0900

    add function rule_vertical

--- solver/srcs/rule.c
+++ solver/srcs/rule.c
@@ -8,12 +8,31 @@ int rule_horizontal(int map[9][9])
         {
             if (map[i][j] != 0)
             {
-                // printf("j = %d\n", j);
                 for (int k = 0; k < 9; k++)
                 {
                     if (k != j && map[i][j] == map[i][k])
                     {
-                        // printf("k = %d\n", k);
+                        return (0);
+                    }
+                }
+            }
+        }
+    }
+    return (1);
+}
+
+int rule_vertical(int map[9][9])
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (map[j][j] != 0)
+            {
+                for (int k = 0; k < 9; k++)
+                {
+                    if (k != j && map[j][i] == map[k][i])
+                    {
                         return (0);
                     }
                 }
